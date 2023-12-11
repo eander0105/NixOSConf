d3dcfccfbec8594b12ae4cf4eb99664484f40753
Emil Andersson <eander0105@gmail.com>
2023-12-11 19:10:08 +0100

    added gnome extension unite

--- modules/nixos/UI/gnome.nix
+++ modules/nixos/UI/gnome.nix
@@ -14,6 +14,7 @@
         gnomeExtensions.gsconnect
         gnomeExtensions.workspace-matrix
         gnomeExtensions.auto-move-windows
+        gnomeExtensions.unite
       ];
 
       gnome.excludePackages = (with pkgs; [
